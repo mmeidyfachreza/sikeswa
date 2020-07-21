ea38a91b4a0b123b8912f41f2b1907b7348f2646
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-07-22 02:05:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,19 @@ Route::get('/', function () {
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['admin'], 'prefix' => 'admin/surat'], function () {
+    Route::get('surat-rujukan-kesehatan-anak-sekolah', 'letterController@indexSRKAS')->name('letter.index.srkas');
+    Route::post('surat-rujukan-kesehatan-anak-sekolah', 'letterController@printSRKAS')->name('letter.print.srkas');
+    Route::get('surat-rujukan-balik', 'letterController@indexRujukBlk')->name('letter.index.rujukblk');
+    Route::post('surat-rujukan-balik', 'letterController@printRujukBlk')->name('letter.print.rujukblk');
+    Route::get('surat-rujukan-UKS', 'letterController@indexRujukUKS')->name('letter.index.rujukuks');
+    Route::post('surat-rujukan-UKS', 'letterController@printRujukUKS')->name('letter.print.rujukuks');
+    Route::get('kartu-kontrol','DashboardController@indexkk')->name('karkon.index');
+    Route::post('kartu-kontrol','DashboardController@cetak')->name('karkon.cetak');
+});
 
-Route::group(['middleware'=>'admin'], function () {
-    Route::get('/admin','DashboardController@index');
+Route::group(['middleware'=>'admin', 'prefix' => 'admin'], function () {
+    Route::get('/','DashboardController@index');
     Route::resource('user', 'UserController');
     Route::resource('siswa', 'StudentController');
     Route::resource('kesehatan', 'HealthController');
@@ -34,27 +44,17 @@ Route::group(['middleware'=>'admin'], function () {
     Route::resource('status-gizi', 'MeasurementController');
     Route::resource('rekam-medik', 'MedicalRecordController');
     Route::resource('kelas', 'ClassroomController');
+    
+    Route::get('siswa-export','StudentController@export')->name('siswa.export');
 
-    Route::get('surat/surat-rujukan-kesehatan-anak-sekolah', 'letterController@indexSRKAS')->name('letter.index.srkas');
-    Route::post('surat/surat-rujukan-kesehatan-anak-sekolah', 'letterController@printSRKAS')->name('letter.print.srkas');
-    Route::get('surat/surat-rujukan-balik', 'letterController@indexRujukBlk')->name('letter.index.rujukblk');
-    Route::post('surat/surat-rujukan-balik', 'letterController@printRujukBlk')->name('letter.print.rujukblk');
-    Route::get('surat/surat-rujukan-UKS', 'letterController@indexRujukUKS')->name('letter.index.rujukuks');
-    Route::post('surat/surat-rujukan-UKS', 'letterController@printRujukUKS')->name('letter.print.rujukuks');
-
-    Route::get('yajra', 'HealthController@yajra')->name('kesehatan.yajra');
-    Route::get('kesehatan/student/{id}', 'HealthController@StudentFindHealth')->name('student.find.health');
+    Route::get('kesehatan/student/{id}', 'HealthController@indexHealth')->name('student.find.health');
     Route::get('kesehatan/create/{id}', 'HealthController@create')->name('kesehatan2.create');
 
-    Route::get('imunisasi/student/{id}', 'ImmunizationHistoryController@StudentFindImmune')->name('student.find.immune');
+    Route::get('imunisasi/student/{id}', 'ImmunizationHistoryController@indexImmune')->name('student.find.immune');
     Route::get('imunisasi/create/{id}', 'ImmunizationHistoryController@create')->name('imunisasi2.create');
 
-    Route::get('rekam-medik/student/{id}', 'MedicalRecordController@StudentFindRecord')->name('student.find.record');
+    Route::get('rekam-medik/student/{id}', 'MedicalRecordController@indexMedRec')->name('student.find.record');
     Route::get('rekam-medik/create/{id}', 'MedicalRecordController@create')->name('rekam-medik2.create');
-
-    Route::post('kesehatan/cari_siswa', 'HealthController@SearchStudent')->name('student.search.health');
-    Route::post('imunisasi/cari_siswa', 'ImmunizationHistoryController@SearchStudent')->name('student.search.immune');
-    Route::post('rekam-medik/cari_siswa', 'MedicalRecordController@SearchStudent')->name('student.search.record');
 
     Route::post('users/update', 'HomeController@tested');
 });
