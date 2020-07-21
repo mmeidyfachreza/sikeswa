5a5f529686f8f5e79e0aeb3114ec1898c519a9a6
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-07-22 01:08:55 +0800

    Update Document.php

--- app/Document.php
+++ app/Document.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $fillable = [
+        'uuid',
+        'nama',
+        'namafile',
+        'tahun',
+        'publikasi'
+    ]
 }
