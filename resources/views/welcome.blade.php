0b5cd4f64bd7c32cf309a19de30c37b568621929
mmeidyfachreza <meidyfachreza@gmail.com>
2022-07-13 23:03:11 +0800

    change name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Company Bootstrap Template - Index</title>
+  <title>{{ Str::upper(config('app.name', 'mumefa-project')) }}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
