d00a23d039a8d0a305479d2352c452b8a0cefc6b
mmeidyfachreza <meidyfachreza@gmail.com>
2022-07-16 20:50:50 +0800

    clean code

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,89 +3,89 @@
 @section('content')
 <section class="dashboard-counts section-padding">
     <div class="container-fluid">
-      <div class="row">
-        <!-- Count item widget-->
-        <div class="col-xl-2 col-md-4 col-6">
-          <div class="wrapper count-title d-flex">
-            <div class="icon"><i class="icon-user"></i></div>
-            <div class="name"><strong class="text-uppercase">Total Anak</strong>
-              <div class="count-number">{{$total_student}}</div>
+        <div class="row">
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+                <div class="wrapper count-title d-flex">
+                    <div class="icon"><i class="icon-user"></i></div>
+                    <div class="name"><strong class="text-uppercase">Total Anak</strong>
+                        <div class="count-number">{{$total_student}}</div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <!-- Count item widget-->
-        <div class="col-xl-2 col-md-4 col-6">
-            <div class="wrapper count-title d-flex">
-              <div class="icon"><i class="icon-user"></i></div>
-              <div class="name"><strong class="text-uppercase">Anak Laki-laki</strong>
-                <div class="count-number">{{$total_boy}}</div>
-              </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+                <div class="wrapper count-title d-flex">
+                    <div class="icon"><i class="icon-user"></i></div>
+                    <div class="name"><strong class="text-uppercase">Anak Laki-laki</strong>
+                        <div class="count-number">{{$total_boy}}</div>
+                    </div>
+                </div>
             </div>
-          </div>
-        <!-- Count item widget-->
-        <div class="col-xl-2 col-md-4 col-6">
-            <div class="wrapper count-title d-flex">
-              <div class="icon"><i class="icon-user"></i></div>
-              <div class="name"><strong class="text-uppercase">Anak Perempuan</strong>
-                <div class="count-number">{{$total_girl}}</div>
-              </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+                <div class="wrapper count-title d-flex">
+                    <div class="icon"><i class="icon-user"></i></div>
+                    <div class="name"><strong class="text-uppercase">Anak Perempuan</strong>
+                        <div class="count-number">{{$total_girl}}</div>
+                    </div>
+                </div>
             </div>
-          </div>
-        <!-- Count item widget-->
-        <div class="col-xl-2 col-md-4 col-6">
+            <!-- Count item widget-->
+            {{-- <div class="col-xl-2 col-md-4 col-6">
           <div class="wrapper count-title d-flex">
             <div class="icon"><i class="icon-list-1"></i></div>
             <div class="name"><strong class="text-uppercase">Tidak Diketahui</strong><span>Tidak Diketahui</span>
               <div class="count-number">0</div>
             </div>
           </div>
+        </div> --}}
         </div>
-      </div>
     </div>
-  </section>
-  <!-- Header Section-->
-  <section class="dashboard-header section-padding">
+</section>
+<!-- Header Section-->
+<section class="dashboard-header section-padding">
     <div class="container-fluid">
-      <div class="row d-flex align-items-md-stretch">
+        <div class="row d-flex align-items-md-stretch">
 
-        <!-- Line Chart -->
-        <div class="col-lg-6 col-md-12 flex-lg-last flex-md-first align-self-baseline">
-            <div class="card sales-report">
-              <h2 class="display h4">Grafik Tinggi Badan Anak</h2>
-              <p>Laki-laki dan Perempuan</p>
-              <div class="line-chart">
-                <canvas id="lineCahrt"></canvas>
-              </div>
+            <!-- Line Chart -->
+            <div class="col-lg-6 col-md-12 flex-lg-last flex-md-first align-self-baseline">
+                <div class="card sales-report">
+                    <h2 class="display h4">Grafik Tinggi Badan Anak</h2>
+                    <p>Laki-laki dan Perempuan</p>
+                    <div class="line-chart">
+                        <canvas id="lineCahrt"></canvas>
+                    </div>
+                </div>
             </div>
-          </div>
-        <!-- Pie Chart-->
-        <div class="col-lg-3 col-md-6">
-          <div class="card project-progress">
+            <!-- Pie Chart-->
+            <div class="col-lg-3 col-md-6">
+                {{-- <div class="card project-progress">
             <h2 class="display h4">Project Beta progress</h2>
             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             <div class="pie-chart">
               <canvas id="pieChart" width="300" height="300"> </canvas>
             </div>
-          </div>
+          </div> --}}
+            </div>
         </div>
-      </div>
     </div>
-  </section>
-  <!-- Statistics Section-->
-  <section class="statistics">
+</section>
+<!-- Statistics Section-->
+<section class="statistics">
     <div class="container-fluid">
-      <div class="row d-flex">
-        <div class="col-lg-4">
-          <!-- Income-->
-          <div class="card income text-center">
+        <div class="row d-flex">
+            <div class="col-lg-4">
+                <!-- Income-->
+                {{-- <div class="card income text-center">
             <div class="icon"><i class="icon-line-chart"></i></div>
             <div class="number">126,418</div><strong class="text-primary">All Income</strong>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <!-- Monthly Usage-->
-          <div class="card data-usage">
+          </div> --}}
+            </div>
+            <div class="col-lg-4">
+                <!-- Monthly Usage-->
+                {{-- <div class="card data-usage">
             <h2 class="display h4">Monthly Usage</h2>
             <div class="row d-flex align-items-center">
               <div class="col-sm-6">
@@ -94,11 +94,11 @@
               <div class="col-sm-6"><strong class="text-primary">80.56 Gb</strong><small>Current Plan</small><span>100 Gb Monthly</span></div>
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <!-- User Actibity-->
-          <div class="card user-activity">
+          </div> --}}
+            </div>
+            <div class="col-lg-4">
+                <!-- User Actibity-->
+                {{-- <div class="card user-activity">
             <h2 class="display h4">User Activity</h2>
             <div class="number">210</div>
             <h3 class="h4 display">Social Users</h3>
@@ -109,11 +109,11 @@
               <div class="page-statistics-left"><span>Pages Visits</span><strong>230</strong></div>
               <div class="page-statistics-right"><span>New Visits</span><strong>73.4%</strong></div>
             </div>
-          </div>
+          </div> --}}
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 @endsection
 
 @section('script')
@@ -121,5 +121,3 @@
 <script src="{{asset('template/js/charts-home.js')}}"></script>
 <script src="{{asset('template/js/home-premium.js')}}"> </script>
 @endsection
-
-
