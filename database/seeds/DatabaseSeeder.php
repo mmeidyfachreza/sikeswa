269665ed55650e7e0dc693354bccb42ff268eac3
mmeidyfachreza <meidyfachreza@gmail.com>
2022-08-03 23:37:25 +0800

    add health screeaning seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MeasurementsTableSeeder::class);
 
         //medical record form seeder
+        $this->call(HealthScreeningTableSeeder::class);
         $this->call(MentalHealthTableSeeder::class);
         $this->call(MentalHealth2TableSeeder::class);
         $this->call(LearningModalitiesTableSeeder::class);
